5deb4ba85467d2eaa8a5b3dcbb5d6861d96dde69
Guillaume-Helbecque <helbecque.guillaume@gmail.com>
2024-10-25 19:14:36 +0200

    add resources for lecture on PGAS

---
+++ 6-pgas-chapel/chapel_install.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Configuration and installation of Chapel for multi-core experiments on laptop.
+
+export HERE=$(pwd)
+
+export CHPL_VERSION=2.2.0
+export CHPL_HOME=$HERE/chapel-${CHPL_VERSION}MC
+
+# Download Chapel if not found
+if [ ! -d "$CHPL_HOME" ]; then
+  wget -c https://github.com/chapel-lang/chapel/releases/download/$CHPL_VERSION/chapel-${CHPL_VERSION}.tar.gz -O - | tar xz
+  mv chapel-$CHPL_VERSION $CHPL_HOME
+fi
+
+CHPL_BIN_SUBDIR=`"$CHPL_HOME"/util/chplenv/chpl_bin_subdir.py`
+export PATH="$PATH":"$CHPL_HOME/bin/$CHPL_BIN_SUBDIR"
+export MANPATH="$MANPATH":"$CHPL_HOME"/man
+
+export CHPL_HOST_PLATFORM=`$CHPL_HOME/util/chplenv/chpl_platform.py`
+# export CHPL_HOST_COMPILER=gnu
+export CHPL_TARGET_ARCH=native
+export CHPL_LLVM=none
+NUM_T_LOCALE=$(cat /proc/cpuinfo | grep processor | wc -l)
+export CHPL_RT_NUM_THREADS_PER_LOCALE=$NUM_T_LOCALE
+
+cd $CHPL_HOME
+make -j $NUM_T_LOCALE
+cd $HERE
